8e7b582780ade7623a213c7a320f3e66d76d2560
Thakur-786 <thakursantoshsing.bajajfinserv.in>
2021-07-23 09:38:52 +0530

    First flutter app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    Center(
+ child: Text("Hello World",textDirection: TextDirection.ltr,
+   style: TextStyle(color: Colors.lime,fontSize: 25.0),)
+ ),
+  );
+}
